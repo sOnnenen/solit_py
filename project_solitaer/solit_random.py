8675ec9a9119dd57cc961f44e482f59ef068a9b9
Oliver Schneider <o.schneider1@oth-aw.de>
2020-03-18 15:59:10 +0100

    playing random moves in solitaer

---
+++ project_solitaer/solit_random.py
@@ -0,0 +1,56 @@
+import numpy as np
+import random
+
+board = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],[0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],[0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],[0, 0, 2, 2, 2, 1, 2, 2, 2, 0, 0],[0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],[0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],[0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+def find_valid_moves():
+    presentPins = np.asarray(np.where(board == 2)).T
+    list_of_valid_moves_2 = []
+    for count, element in enumerate(presentPins):
+        directions = find_possible_directions(element)
+        if len(directions) != 0:
+            list_of_valid_moves_2.append([element, directions])
+
+    return list_of_valid_moves_2
+
+def move(pin_position, direction):
+
+    board[pin_position[0]][pin_position[1]] = 1
+    if direction == "up":
+        board[pin_position[0]-1][pin_position[1]] = 1
+        board[pin_position[0]-2][pin_position[1]] = 2
+
+    if direction == "down":
+        board[pin_position[0]+1][pin_position[1]] = 1
+        board[pin_position[0]+2][pin_position[1]] = 2
+
+    if direction == "left":
+        board[pin_position[0]][pin_position[1]-1] = 1
+        board[pin_position[0]][pin_position[1]-2] = 2
+
+    if direction == "right":
+        board[pin_position[0]][pin_position[1]+1] = 1
+        board[pin_position[0]][pin_position[1]+2] = 2
+
+def find_possible_directions(x):
+    list_of_directions = []
+    if board[x[0]][x[1]+1] == 2 and board[x[0]][x[1]+2] == 1:
+        list_of_directions.append('right')
+    if board[x[0]][x[1]-1] == 2 and board[x[0]][x[1]-2] == 1:
+        list_of_directions.append('left')
+    if board[x[0]+1][x[1]] == 2 and board[x[0]+2][x[1]] == 1:
+        list_of_directions.append('down')
+    if board[x[0]-1][x[1]] == 2 and board[x[0]-2][x[1]] == 1:
+        list_of_directions.append('up')
+    return list_of_directions
+
+gamestate = True
+while gamestate:
+    print(board, "\n")
+    list = find_valid_moves()
+    if len(list) == 0:
+        break
+    nextmove = (list[random.randint(0,len(list)-1)])
+    orientation = nextmove[1]
+    move(nextmove[0],orientation[random.randint(0,len(orientation)-1)])
+print(board, "\n")
