b981758b730fcf026effda62f09ee2b09f3de2d1
schneidero1 <o.schneider1@oth-aw.de>
2021-01-17 21:03:47 +0100

    fixed typo

--- project_solitaer/plot_5_static.py
+++ project_solitaer/plot_5_static.py
@@ -25,7 +25,7 @@ alpha = 1
 gamma = 1
 epsilon_start = [0.01, 0.00875, 0.0075, 0.00625, 0.005]  # 0.99 # bei keinem decay irrelevant
 epsilon_end = 0.0005  # 0.005
-alpha_end = 0.0005
+alpha_end = 0.001
 
 """
 Spielbrett aufsetzten
